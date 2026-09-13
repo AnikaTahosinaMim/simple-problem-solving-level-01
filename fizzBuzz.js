ba124adae7e7e1aab756678d6b1a1254cca1c207
anika557 <ummehabiba19758@gmail.com>
2026-09-13 10:16:00 +0600

    FizzBuzz

---
+++ fizzBuzz.js
@@ -0,0 +1,12 @@
+const FizzBuzz = (nums) => {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    }
+  }
+};
+FizzBuzz();
